82072adb46e79c9ea225dcf81fe78a7854c90f32
jaolson88 <olsonja88@gmail.com>
2023-08-07 13:05:59 -0500

    Fix: Make Category and Post models.

--- backend/src/main/java/com/olsonja/portfolio2/model/Post.java
+++ backend/src/main/java/com/olsonja/portfolio2/model/Post.java
@@ -1,4 +1,43 @@
 package com.olsonja.portfolio2.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Post {
+
+    // Instance Variables
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
