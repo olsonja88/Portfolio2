018d74f2190e3c7e2bc50eed841189f8ece07d71
jaolson88 <olsonja88@gmail.com>
2023-09-20 16:20:44 -0500

    Setup project for local development.

--- frontend/portfolio-app/src/config.js
+++ frontend/portfolio-app/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://frozen-dusk-55881-7ff3d5645692.herokuapp.com/api';
-//const API_BASE_URL = 'http://localhost:8080/api'
+//const API_BASE_URL = 'https://frozen-dusk-55881-7ff3d5645692.herokuapp.com/api';
+const API_BASE_URL = 'http://localhost:8080/api'
 
 export { API_BASE_URL };
